3c9cb12e704f3c20188b452b38d9008f1cdeef03
709637 <vipul.winfo21@gmail.com>
2020-04-06 00:23:57 +0530

    Redux example added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CompTwo from './CustomComponents/CompTwo/CompTwo.js';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import {storeApi} from './redux/store';
+import CakeContainer from './CustomComponents/cakeContainer';
+import TotalItemContainer from './CustomComponents/totalItemCount';
+import TotalItemLeft from './CustomComponents/TotalItemLeft';
+import NumberOfTeamsGet from './CustomComponents/NumberOfTeamsGet';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>Name :  </label><input type="text"></input><br/>
+          <label>Age :  </label><input type="text"></input>   
+         </form>
       </header>
+      {/* <CakeContainer></CakeContainer> */}
+      {/* <TotalItemContainer></TotalItemContainer> */}
+      <TotalItemLeft></TotalItemLeft>
+      <NumberOfTeamsGet></NumberOfTeamsGet>
     </div>
+    </Provider>
   );
 }
 
